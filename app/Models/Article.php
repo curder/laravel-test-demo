4c400d12ad1ce20eb172271beab928c35106b981
curder <q.curder@gmail.com>
2021-02-09 10:56:41 +0800

    test eloquent model

---
+++ app/Models/Article.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Query\Builder;
+
+/**
+ * Class Article
+ * @method static trending()
+ *
+ * @package App\Models
+ */
+class Article extends Model
+{
+    use HasFactory;
+
+    public function scopeTrending(\Illuminate\Database\Eloquent\Builder $query, $take = 3)
+    {
+        return $query->orderByDesc('reads')->take($take)->get();
+    }
+}
---
+++ tests/Unit/Integration/Models/ArticleTest.php
@@ -0,0 +1,32 @@
+<?php
+namespace Tests\Unit\Integration\Models;
+
+use App\Models\Article;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+/**
+ * Class ArticleTest
+ *
+ * @package Tests\Unit\Integration\Models
+ */
+class ArticleTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_fetches_trending_articles() : void
+    {
+        // Given
+        Article::factory(2)->create();
+        Article::factory()->create(['reads' => 10]);
+        $mostPopular = Article::factory()->create(['reads' => 20]);
+
+        // When
+        $articles = Article::trending();
+
+        // Then
+        self::assertEquals($mostPopular->id, $articles->first()->id);
+        self::assertCount(3, $articles);
+    }
+}
